9ac1756937d4b030ccc02857cd263c48ea549a50
Alan Rynne <alan@rynne.es>
2023-10-27 11:12:44 +0200

    feat: Add return to Program.cs Main call

--- SpeckleAutomateDotnetExample/Program.cs
+++ SpeckleAutomateDotnetExample/Program.cs
@@ -1,6 +1,6 @@
 ﻿using Speckle.Automate.Sdk;
 
 // WARNING do not delete this call, this is the actual execution of your function
-await AutomationRunner
+return await AutomationRunner
   .Main<FunctionInputs>(args, AutomateFunction.Run)
   .ConfigureAwait(false);
