062e08b1b228f3b1055f55c80e84dd2a7321140b
Jedd Morgan <45512892+JR-Morgan@users.noreply.github.com>
2025-05-28 23:37:47 +0100

    First pass

--- SpeckleAutomateDotnetExample/Program.cs
+++ SpeckleAutomateDotnetExample/Program.cs
@@ -1,6 +1,13 @@
-﻿using Speckle.Automate.Sdk;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Speckle.Automate.Sdk;
+
+var serviceCollection = new ServiceCollection();
+serviceCollection.AddAutomateSdk();
+serviceCollection.AddSingleton<AutomateFunction>();
+await using var container = serviceCollection.BuildServiceProvider();
+
+var runner = container.GetRequiredService<IAutomationRunner>();
+var function = container.GetRequiredService<AutomateFunction>();
 
 // WARNING do not delete this call, this is the actual execution of your function
-return await AutomationRunner
-  .Main<FunctionInputs>(args, AutomateFunction.Run)
-  .ConfigureAwait(false);
+return await runner.Main<FunctionInputs>(args, function.Run);
